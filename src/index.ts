2103d51891574f0f09ef08205ceb8ad24f6265c7
youruichao <youruichao@souche.com>
2019-09-30 22:08:31 +0800

    feat: make it work

--- src/index.ts
+++ src/index.ts
@@ -17,31 +17,19 @@ function makeMDListItem(issue: Issue) {
 
 interface IConfig {
   link: string
-  accessToken: string
+  accessToken?: string
 }
 
 export default async function generate({ link, accessToken }: IConfig) {
-  assert(accessToken, 'should provide accessToken')
+  // assert(accessToken, 'should provide accessToken')
   const path = url.parse(link).path
 
   assert(path, 'Link should has path')
   if (!path) return
   const paths = path.split('/').filter(Boolean)
-  console.log('paths.length :', paths.length)
   assert(paths.length === 2, 'Link should has path')
   const [owner, repo] = paths
-  const api = `${GITHUB_API}/repos/${owner}/${repo}/issues?access_token=${accessToken}`
+  const api = `${GITHUB_API}/repos/${owner}/${repo}/issues`
   const response = await axios.get<Issue[]>(api)
   return response.data.map(issue => makeMDListItem(issue)).join('\n')
 }
-
-// const accessToken = 'dbfe3dd66ee904988f2baaaf9bf49a03b6e3fd62'
-// const link = 'https://www.github.com/hanxi/blog'
-// ;(async () => {
-//   console.log(
-//     await generate({
-//       link,
-//       accessToken
-//     })
-//   )
-// })()
